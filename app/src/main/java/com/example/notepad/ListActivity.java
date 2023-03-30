0d0402dc1f11bf559cc19df117de6f95fb8f419d
Texa-Dev <artemtroc26@gmail.com>
2023-03-30 23:03:02 +0300

    add DBManager

---
+++ app/src/main/java/com/example/notepad/ListActivity.java
@@ -0,0 +1,26 @@
+package com.example.notepad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notepad.data.DBManager;
+import com.example.notepad.data.Note;
+
+import java.time.LocalDateTime;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        DBManager dbManager = new DBManager(this);
+        dbManager.createTab();
+        dbManager.saveNote(new Note(0,"test","text", LocalDateTime.now()));
+        dbManager.findAll();
+
+       // dbManager.dropTab();
+    }
+}
